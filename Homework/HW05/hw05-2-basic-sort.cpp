588238b17c5e926f89bed835668dd830a7848ca5
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-10 01:55:48 +0700

    in progress

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -1,6 +1,6 @@
 /*
     จงเรียงลำดับเลขจากข้อมูลที่ผู้ใช้กรอก โดยเรียงจากมากไปน้อย (โดยใช้คำสั่ง if else หรือ else if เท่านั้น)
-    
+
     Test case:
         Input[1] :
             6
@@ -21,3 +21,50 @@
     Output:
         112 91 65
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int Ip1, Ip2, Ip3;
+
+    printf("Input[1] :\n");
+    scanf("%d", &Ip1);
+    printf("Input[2] :\n");
+    scanf("%d", &Ip2);
+    printf("Input[3] :\n");
+    scanf("%d", &Ip3);
+
+    printf("Output:\n");
+
+    for (int i; i <= 3; i++)
+    {
+
+        if (Ip1 > Ip2 || Ip1 > Ip3)
+        {
+            printf("%d %d %d", Ip1, Ip2, Ip3);
+        }
+
+        else if (Ip2 > Ip1 || Ip2 > Ip3)
+        {
+            printf("%d %d %d", Ip2, Ip1, Ip3);
+        }
+
+        else if (Ip3 > Ip1 || Ip3 > Ip2)
+        {
+            printf("%d %d %d", Ip3, Ip2, Ip1);
+        }
+    }
+
+    // else if (Ip3 > Ip1 || Ip3 > Ip2)
+    // {
+    //     printf("%d %d %d", Ip3, Ip2, Ip1);
+    // }
+
+    // else if (Ip3 > Ip1 || Ip3 > Ip2)
+    // {
+    //     printf("%d %d %d", Ip3, Ip2, Ip1);
+    // }
+
+    return 0;
+}
